9a13ad9efb6b906f5825b7dffadd07549bd232a0
Batuhan Güneş <batuhangunes001@gmail.com>
2020-05-04 02:10:19 +0300

    Delete personal data

--- dpudak.com/Controllers/DataBaseController.cs
+++ dpudak.com/Controllers/DataBaseController.cs
@@ -21,12 +21,6 @@ namespace dpudak.com.Controllers
             return View();
         }
 
-        public ActionResult EmreKuruoglu()
-        {
-            return View();
-        }
-
-
         public ActionResult DownloadFile()
         {
             string path = AppDomain.CurrentDomain.BaseDirectory + "css/";
